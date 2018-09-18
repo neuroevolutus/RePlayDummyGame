86f1f6f872877000ac41bccdb98d01f993a93d89
Jacob Brunson <jacob@brunson.me>
2018-09-18 13:51:48 -0500

    used intent extra instead of activity onstructor to pass content directory

--- DummyGame/Activity1.cs
+++ DummyGame/Activity1.cs
@@ -14,17 +14,17 @@ namespace DummyGame.Android
         , ScreenOrientation = ScreenOrientation.FullUser
         , ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.Keyboard | ConfigChanges.KeyboardHidden | ConfigChanges.ScreenSize)]
     public class Activity1 : Microsoft.Xna.Framework.AndroidGameActivity
-    {
-        private string contentDir;
-
-        public Activity1(string contentDir = "Content")
-        {
-            this.contentDir = contentDir;
-        }
-
+    {              
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
+
+            string contentDir = "Content";
+            if (Intent.HasExtra("CONTENT_DIR"))
+            {
+                contentDir = Intent.GetStringExtra("CONTENT_DIR");
+            }
+
             var g = new Game1(contentDir);
             SetContentView((View)g.Services.GetService(typeof(View)));
             g.Run();
